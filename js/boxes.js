e9bd6e16b22a2a9aac58ee0ed63a05e516fde09c
Abdelhady Salah <hadysalah1455@gmail.com>
2022-05-23 00:21:17 +0200

    Convert box expand effect to JS

---
+++ js/boxes.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    const main = document.querySelector('main')
+    main.addEventListener('click', (e) => {
+        const parent = e.target.parentElement
+        if (parent.tagName === 'DIV' && parent.classList.contains('box')) {
+            const boxContent = parent.querySelector('span.box-content')
+            boxContent.style.display = getComputedStyle(boxContent).display === 'block' ? 'none' : 'block'
+        }
+    }, true)
+}
